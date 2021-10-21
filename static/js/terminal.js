dce13b78be8d78ff192db1dbdb60a7c00ea8f2e9
Anna Lin <alin@mitre.org>
2021-10-21 02:06:16 -0400

    Got xTerm working again

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -1,53 +1,57 @@
+import * as Terminal from './xterm.js';
+
 // build terminal emulator
-let prompt = "~$ ";
-let term = new Terminal();
+let prompt = '~$ ';
+let term = new window.Terminal();
 term.setOption('cursorBlink', true);
 term.open(document.getElementById('xterminal'));
 
 // run terminal emulator
-let input = "";
+let input = '';
 let shellHistory = [];
 let shellHistoryIndex = 0;
-term.onData(function(data) {
- const code = data.charCodeAt(0);
- if(code === 13) {
-     if (input !== "" && !checkSpecialKeywords(input)) {
-         runCommand(input);
-         shellHistory.pop();
-         shellHistory.push(input);
-         shellHistory.push("");
-         shellHistoryIndex = shellHistory.length - 1;
-     } else {
-         term.write('\r\n' + prompt + ' ');
-     }
-     input = "";
- } else if (code === 127) {
-     if (input.length > 0) {
-         term.write("\b \b");
-         input = input.substr(0, input.length - 1);
-     }
-     return;
- } else if (code === 27) {
-    updateHistory(data);
- } else if (code < 32) {
-     return;
- } else {
-     term.write(data);
-     input += data;
- }
+
+term.write(prompt);
+
+term.onData((data) => {
+    const code = data.charCodeAt(0);
+    if (code === 13) {
+        if (input !== '' && !checkSpecialKeywords(input)) {
+            runCommand(input);
+            shellHistory.pop();
+            shellHistory.push(input);
+            shellHistory.push('');
+            shellHistoryIndex = shellHistory.length - 1;
+        } else {
+            term.write(`\r\n ${prompt} `);
+        }
+        input = '';
+    } else if (code === 127) {
+        if (input.length > 0) {
+            term.write('\b \b');
+            input = input.substr(0, input.length - 1);
+        }
+    } else if (code === 27) {
+        updateHistory(data);
+    } else if (code < 32) {
+        return;
+    } else {
+        term.write(data);
+        input += data;
+    }
 });
 
 function updateHistory(data) {
     if (shellHistory.length > 0) {
-        if (data.localeCompare("[A") === 0) {
+        if (data.localeCompare('[A') === 0) {
             handleUpArrow();
-        } else if (data.localeCompare("[B") === 0){
+        } else if (data.localeCompare('[B') === 0) {
             handleDownArrow();
         }
     }
 }
 
-function handleUpArrow(){
+function handleUpArrow() {
     if (shellHistoryIndex > 0) {
         shellHistoryIndex--;
         writeHistory(shellHistory[shellHistoryIndex]);
@@ -55,115 +59,118 @@ function handleUpArrow(){
 }
 
 function handleDownArrow() {
-    if (shellHistoryIndex < shellHistory.length - 1){
+    if (shellHistoryIndex < shellHistory.length - 1) {
         shellHistoryIndex++;
         writeHistory(shellHistory[shellHistoryIndex]);
     }
 }
 
 function writeHistory(value) {
-    term.write('\33[2K\r' + prompt + " ");
+    term.write(`^[[2K\r ${prompt} `);
     term.write(value);
     input = value;
 }
 
 function getShellHistory(elem) {
-    restRequest('POST', {'paw':elem.options[elem.selectedIndex].getAttribute('data-paw')}, populateHistory, endpoint='/plugin/manx/history');
+    restRequest('POST', { 'paw': elem.options[elem.selectedIndex].getAttribute('data-paw') }, populateHistory, endpoint = '/plugin/manx/history');
 }
 
 function populateHistory(data) {
-    if (data.length > 0){
+    if (data.length > 0) {
         for (let index in data) {
             shellHistory.push(data[index]['cmd']);
         }
-        shellHistory.push("");
+        shellHistory.push('');
         shellHistoryIndex = shellHistory.length - 1;
     }
 }
 
 function displayHistory() {
     for (let i = 0; i < shellHistory.length - 1; i++) {
-        term.write('\r\n['+i+']  ' +shellHistory[i]);
+        term.write('\r\n[' + i + ']  ' + shellHistory[i]);
     }
 }
 
 function checkSpecialKeywords(word) {
-    if (word.localeCompare("history") === 0) {
-        console.log("displaying history");
+    if (word.localeCompare('history') === 0) {
+        console.log('displaying history');
         displayHistory();
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
 function runCommand(input) {
- let sessionId = $('#session-id option:selected').attr('value');
- var [wsHost, wsPort] = $('#websocket-data').data('websocket').split(':');
+    let sessionId = $('#session-id option:selected').attr('value');
+    var [wsHost, wsPort] = $('#websocket-data').data('websocket').split(':');
 
- if (wsHost === '0.0.0.0'){
-    console.log('WebSocket host configured for 0.0.0.0. Using window location for host/ip instead.');
-    wsHost = window.location.hostname;
- }
+    if (wsHost === '0.0.0.0') {
+        console.log('WebSocket host configured for 0.0.0.0. Using window location for host/ip instead.');
+        wsHost = window.location.hostname;
+    }
 
- var wsProto = (location.protocol == 'https:') ? 'wss://' : 'ws://';
+    var wsProto = (location.protocol == 'https:') ? 'wss://' : 'ws://';
 
- var socket = new WebSocket(wsProto + wsHost + ':' + wsPort + '/manx/' + sessionId);
+    var socket = new WebSocket(wsProto + wsHost + ':' + wsPort + '/manx/' + sessionId);
 
- socket.onopen = function () {
-     socket.send(input);
- };
+    socket.onopen = function () {
+        socket.send(input);
+    };
 
- socket.onmessage = function (s) {
-     try {
-         let jData = JSON.parse(s.data);
-         let lines = jData["response"].split('\n');
-         for(let i = 0;i < lines.length;i++){
-            term.write("\r\n"+lines[i]);
-         }
-         prompt = jData["pwd"];
-         term.write("\r\n"+prompt+'$ ');
-     } catch(err){
-         term.write("\r\n"+'Dead session. Probably. It has been removed.');
-         clearTerminal();
-         $('#session-id option:selected').remove();
-     }
- };
+    socket.onmessage = function (s) {
+        try {
+            let jData = JSON.parse(s.data);
+            let lines = jData['response'].split('\n');
+            for (let i = 0; i < lines.length; i++) {
+                term.write('\r\n' + lines[i]);
+            }
+            prompt = jData['pwd'];
+            term.write('\r\n' + prompt + '$ ');
+        } catch (err) {
+            term.write('\r\n' + 'Dead session. Probably. It has been removed.');
+            clearTerminal();
+            $('#session-id option:selected').remove();
+        }
+    };
 }
 
 // ability filter options
 
 let ABILITIES = [];
 function getAbilities() {
- function getAbilitiesCallback(data){
-    $('#tactic-filter').empty().append("<option disabled='disabled' selected>Choose a tactic</option>");
-     ABILITIES = [];
-     let found = [];
-     data.abilities.forEach(function(ability) {
-         ABILITIES.push(ability);
-         if(!found.includes(ability.tactic)) {
-             $('#tactic-filter').append('<option value="'+ability.tactic+'">'+ability.tactic+'</option>');
-             found.push(ability.tactic);
-         }
-     });
- }
- restRequest('POST', {"paw": $('#session-id option:selected').data('paw')}, getAbilitiesCallback, '/plugin/manx/ability');
+    function getAbilitiesCallback(data) {
+        $('#tactic-filter').empty().append('<option disabled=\'disabled\' selected>Choose a tactic</option>');
+        ABILITIES = [];
+        let found = [];
+        data.abilities.forEach(function (ability) {
+            ABILITIES.push(ability);
+            if (!found.includes(ability.tactic)) {
+                $('#tactic-filter').append('<option value="' + ability.tactic + '">' + ability.tactic + '</option>');
+                found.push(ability.tactic);
+            }
+        });
+    }
+
+    restRequest('POST', { 'paw': $('#session-id option:selected').data('paw') }, getAbilitiesCallback, '/plugin/manx/ability');
 }
+
 function filterTechniques() {
- let found = [];
- $('#technique-filter').empty().append("<option disabled='disabled' selected>Choose a technique</option>");
- ABILITIES.forEach(function(ability){
-     if(ability.tactic === $('#tactic-filter').val() && !found.includes(ability.technique_id)) {
-         $('#technique-filter').append('<option value="'+ability.technique_id+'">'+ability.technique_id+' | '+ability.technique_name+'</option>');
-         found.push(ability.technique_id);
-     }
- });
+    let found = [];
+    $('#technique-filter').empty().append('<option disabled=\'disabled\' selected>Choose a technique</option>');
+    ABILITIES.forEach(function (ability) {
+        if (ability.tactic === $('#tactic-filter').val() && !found.includes(ability.technique_id)) {
+            $('#technique-filter').append('<option value="' + ability.technique_id + '">' + ability.technique_id + ' | ' + ability.technique_name + '</option>');
+            found.push(ability.technique_id);
+        }
+    });
 }
+
 function filterProcedures() {
-    $('#procedure-filter').empty().append("<option disabled='disabled' selected>Choose a procedure</option>");
-        ABILITIES.forEach(function(ability){
-         if(ability.tactic === $('#tactic-filter').val() && ability.technique_id === $('#technique-filter').val()) {
-             $('#procedure-filter').append('<option value="'+ability.ability_id+'">'+ability.name+'</option>');
-         }
+    $('#procedure-filter').empty().append('<option disabled=\'disabled\' selected>Choose a procedure</option>');
+    ABILITIES.forEach(function (ability) {
+        if (ability.tactic === $('#tactic-filter').val() && ability.technique_id === $('#technique-filter').val()) {
+            $('#procedure-filter').append('<option value="' + ability.ability_id + '">' + ability.name + '</option>');
+        }
     });
 }
 function showProcedure() {
@@ -183,14 +190,14 @@ function showProcedure() {
     restRequest('POST', {'index':'abilities','ability_id':$('#procedure-filter').val()}, displayProcedure)
 }
 
-function clearTerminal(){
-    term.write('\33[2K\r');
+function clearTerminal() {
+    term.write(`^[[2K\r`);
     term.clear();
     shellHistory = [];
     shellHistoryIndex = 0;
-    input = "";
+    input = '';
     prompt = '~$ ';
-    term.write("\r\n"+prompt+" ");
+    term.write('\r\n' + prompt + ' ');
 }
 
 //# sourceURL=manxterm.js
