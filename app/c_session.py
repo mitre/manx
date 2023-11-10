839d727b28d4e689f648751b028327c7f79c5472
LetsDrinkSomeTea <104939409+LetsDrinkSomeTea@users.noreply.github.com>
2023-11-10 18:20:34 +0100

    Fixed Issue: Error TCP #2836
    added read and write stream to session class.
    added send and receive methods

--- app/c_session.py
+++ app/c_session.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from app.utility.base_object import BaseObject
 
 
@@ -7,8 +9,10 @@ class Session(BaseObject):
     def unique(self):
         return self.hash('%s' % self.paw)
 
-    def __init__(self, id, paw, connection):
+    def __init__(self, id, paw, connection, reader, writer):
         super().__init__()
+        self._reader: asyncio.StreamReader = reader
+        self._writer: asyncio.StreamWriter = writer
         self.id = id
         self.paw = paw
         self.connection = connection
@@ -19,3 +23,9 @@ class Session(BaseObject):
             ram['sessions'].append(self)
             return self.retrieve(ram['sessions'], self.unique)
         return existing
+
+    def send(self, payload: bytes):
+        self._writer.write(payload)
+
+    async def receive(self, n: int) -> bytes:
+        return await self._reader.read(n=n)
